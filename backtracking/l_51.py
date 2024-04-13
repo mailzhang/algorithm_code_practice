7c67875089647b6e2b74199d8a9e518becb67143
nefulansss <2426398674@qq.com>
2024-04-13 10:00:30 +0800

    八皇后

--- backtracking/l_51.py
+++ backtracking/l_51.py
@@ -8,8 +8,8 @@ class Solution(object):
             if tmp[i][col] == 'Q':
                 return False
         # 左上方
-        r = row
-        c = col
+        r = row - 1
+        c = col - 1
         while r >= 0 and c >= 0:
             if tmp[r][c] == 'Q':
                 return False
@@ -17,8 +17,8 @@ class Solution(object):
             c -= 1
 
         # 右上方
-        r = row
-        c = col
+        r = row - 1
+        c = col + 1
         while r >= 0 and c < len(tmp):
             if tmp[r][c] == 'Q':
                 return False
@@ -32,7 +32,7 @@ class Solution(object):
             if self.is_valid(tmp, row, i):
                 tmp[row][i] = 'Q'
                 if row == len(tmp) - 1:
-                    self.q_list.append(copy.deepcopy(tmp))
+                    self.q_list.append(["".join(_row) for _row in tmp])
                 else:
                     self.backtracking(tmp, row + 1)
 
@@ -44,13 +44,19 @@ class Solution(object):
         :rtype: List[List[str]]
         """
         self.q_list = []
-        tmp = [['*'] * n] * n
+        tmp = [ ['.'] * n for _ in range(n)]
         self.backtracking(tmp, 0)
 
         return self.q_list
 
 
 if __name__ == '__main__':
-    # print([['*'] * 2] * 2)
+    # _s = [['.'] * 2] * 2  # 用这个报错 err
+    # print(_s)
+    # _s = [["."] * 2 for _ in range(2)] # 对的
+    # print(_s)
+    # _s[0][0] = 'Q'
+    # print(_s)
+
     r = Solution().solveNQueens(4)
     print(r)
